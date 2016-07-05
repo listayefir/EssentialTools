9fc018154153d2672b23541d3771b41d0cc778e1
Alisa <filippovaaa18@gmail.com>
2016-07-05 14:25:40 +0400

    Essential tools for MVC developer

---
+++ EssentialTools/Models/ShoppingCart.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ShoppingCart
+    {
+        private ValueCalculator calc;
+
+        public ShoppingCart (ValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        public IEnumerable<Product> Products { get; set; };
+
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+}
---
+++ EssentialTools/Models/ValueCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EssentialTools.Models
+{
+    public class ValueCalculator
+    {
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            return products.Sum(x => x.Price);
+        }
+    }
+}
